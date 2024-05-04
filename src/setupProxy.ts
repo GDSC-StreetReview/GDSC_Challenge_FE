64917d6c7b5db179bd8001bc917fa0bfe229b5b7
KKangHHee <skh8609@gmail.com>
2024-05-04 21:21:49 +0900

    feat: 개발환경에 따른 환경 변수 변경 설정

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,10 +1,9 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-
 module.exports = function (app: any) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://semtle.catholic.ac.kr:8085",
+      target: process.env.REACT_APP_PROXY,
       changeOrigin: true,
     })
   );
